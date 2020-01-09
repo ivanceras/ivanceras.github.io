5d371cab918c214410403977d44110da39c341d6
Jovansonlee Cesar <ivanceras@gmail.com>
2020-01-09 11:53:44 +0800

    Add sample giftcard example

---
+++ gift-cards/pkg/gift_cards_bg.d.ts
@@ -0,0 +1,10 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function initialize(): void;
+export function __wbindgen_malloc(a: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number): number;
+export const __wbindgen_export_2: WebAssembly.Table;
+export function _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h9d5882f83b7f62fc(a: number, b: number, c: number): void;
+export function _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h2310adbdec12dee3(a: number, b: number): void;
+export function __wbindgen_exn_store(a: number): void;
