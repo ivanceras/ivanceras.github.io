734983b4eb6fb3cfee0d81fae5565f6d076a81d8
Jovansonlee Cesar <ivanceras@gmail.com>
2020-07-23 11:08:33 +0800

    new update from svgbob-editor and todomvc

--- todomvc/pkg/todomvc.js
+++ todomvc/pkg/todomvc.js
@@ -482,8 +482,8 @@ async function init(input) {
     imports.wbg.__wbindgen_throw = function(arg0, arg1) {
         throw new Error(getStringFromWasm0(arg0, arg1));
     };
-    imports.wbg.__wbindgen_closure_wrapper126 = function(arg0, arg1, arg2) {
-        var ret = makeMutClosure(arg0, arg1, 36, __wbg_adapter_16);
+    imports.wbg.__wbindgen_closure_wrapper169 = function(arg0, arg1, arg2) {
+        var ret = makeMutClosure(arg0, arg1, 56, __wbg_adapter_16);
         return addHeapObject(ret);
     };
 
