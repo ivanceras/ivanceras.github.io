5ee922641c299aeb400c2a682b7014333048f886
Jovansonlee Cesar <ivanceras@gmail.com>
2023-07-22 02:43:49 +0800

    deploy to alpha

--- bob-editor.alpha/pkg/client_bg.wasm.d.ts
+++ bob-editor.alpha/pkg/client_bg.wasm.d.ts
@@ -2,22 +2,12 @@
 /* eslint-disable */
 export const memory: WebAssembly.Memory;
 export function main(): void;
-export function __wbg_webeditorcustomelement_free(a: number): void;
-export function webeditorcustomelement_new(a: number): number;
-export function webeditorcustomelement_attributeChangedCallback(a: number, b: number, c: number, d: number, e: number): void;
-export function webeditorcustomelement_connectedCallback(a: number): void;
-export function webeditorcustomelement_adoptedCallback(a: number): void;
-export function webeditorcustomelement_register(): void;
-export function webeditorcustomelement_observedAttributes(): number;
-export function webeditorcustomelement_disconnectedCallback(a: number): void;
 export function __wbindgen_export_0(a: number): number;
 export function __wbindgen_export_1(a: number, b: number, c: number): number;
 export const __wbindgen_export_2: WebAssembly.Table;
-export function __wbindgen_export_3(a: number, b: number): void;
-export function __wbindgen_export_4(a: number, b: number, c: number): void;
-export function __wbindgen_export_5(a: number, b: number): void;
-export function __wbindgen_export_6(a: number, b: number, c: number): void;
-export function __wbindgen_export_7(a: number, b: number, c: number): void;
-export function __wbindgen_export_8(a: number, b: number): void;
-export function __wbindgen_export_9(a: number): void;
+export function __wbindgen_export_3(a: number, b: number, c: number): void;
+export function __wbindgen_export_4(a: number, b: number): void;
+export function __wbindgen_export_5(a: number, b: number, c: number): void;
+export function __wbindgen_export_6(a: number, b: number): void;
+export function __wbindgen_export_7(a: number): void;
 export function __wbindgen_start(): void;
