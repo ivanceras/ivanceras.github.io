c8aa37bf353843085657a6ab9e1dea4b7ef2687f
Jovansonlee Cesar <ivanceras@gmail.com>
2023-07-22 23:22:06 +0800

    release to main ultron version

--- ultron/pkg/ultron_app.d.ts
+++ ultron/pkg/ultron_app.d.ts
@@ -3,55 +3,17 @@
 /**
 */
 export function main(): void;
-/**
-*/
-export class WebEditorCustomElement {
-  free(): void;
-/**
-* @param {any} node
-*/
-  constructor(node: any);
-/**
-* @param {string} attr_name
-* @param {any} old_value
-* @param {any} new_value
-*/
-  attributeChangedCallback(attr_name: string, old_value: any, new_value: any): void;
-/**
-*/
-  connectedCallback(): void;
-/**
-*/
-  disconnectedCallback(): void;
-/**
-*/
-  adoptedCallback(): void;
-/**
-*/
-  static register(): void;
-/**
-*/
-  static readonly observedAttributes: any;
-}
 
 export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
 
 export interface InitOutput {
   readonly memory: WebAssembly.Memory;
   readonly main: () => void;
-  readonly __wbg_webeditorcustomelement_free: (a: number) => void;
-  readonly webeditorcustomelement_new: (a: number) => number;
-  readonly webeditorcustomelement_observedAttributes: () => number;
-  readonly webeditorcustomelement_attributeChangedCallback: (a: number, b: number, c: number, d: number, e: number) => void;
-  readonly webeditorcustomelement_connectedCallback: (a: number) => void;
-  readonly webeditorcustomelement_adoptedCallback: (a: number) => void;
-  readonly webeditorcustomelement_register: () => void;
-  readonly webeditorcustomelement_disconnectedCallback: (a: number) => void;
   readonly __wbindgen_export_0: (a: number, b: number) => number;
   readonly __wbindgen_export_1: (a: number, b: number, c: number, d: number) => number;
   readonly __wbindgen_export_2: WebAssembly.Table;
-  readonly __wbindgen_export_3: (a: number, b: number, c: number) => void;
-  readonly __wbindgen_export_4: (a: number, b: number) => void;
+  readonly __wbindgen_export_3: (a: number, b: number) => void;
+  readonly __wbindgen_export_4: (a: number, b: number, c: number) => void;
   readonly __wbindgen_export_5: (a: number, b: number, c: number) => void;
   readonly __wbindgen_export_6: (a: number, b: number, c: number) => void;
   readonly __wbindgen_export_7: (a: number) => void;
